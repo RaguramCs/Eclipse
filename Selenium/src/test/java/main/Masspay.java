eefc18375fd0548d63a0e94c0e3382b6efbf555b
RaguQA <r.anbalagan@bankcbw.org>
2022-05-09 13:16:49 +0530

    masspay file

---
+++ Selenium/src/test/java/main/Masspay.java
@@ -0,0 +1,27 @@
+package main;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class Masspay extends Login{
+	
+	public void masspay() throws InterruptedException {
+		Thread.sleep(3000);
+		WebElement upload = driver.findElement(By.xpath("//a[text()=' Upload ']"));
+		upload.click();
+		WebElement mpay = driver.findElement(By.xpath("//button[text()='MassPay']"));
+		mpay.click();
+	}
+	public static void main(String[] args) throws InterruptedException {
+		Masspay m = new Masspay();
+		m.log();
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
